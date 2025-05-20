ff78eef9025c74207820ef70a22e1da9bae7341e
fishfood65 <167575622+fishfood65@users.noreply.github.com>
2025-05-20 13:46:26 -0700

    Delete page1.py

--- page1.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def page_1():
-    st.subheader("Page 1")
-    st.write("This is the content of Page 1.")
-
-# This function will be called in the main app to load Page 1 content
-if __name__ == "__main__":
-    page_1()
