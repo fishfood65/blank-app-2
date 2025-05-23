f0d66cfe75a57aa4ae89004a2c77fe217202caea
fishfood65 <167575622+fishfood65@users.noreply.github.com>
2025-05-23 08:19:59 -0700

    Update streamlit_app.py by adding home_app_05_23_modified.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ pages = [
     #st.Page("Cat_Bingo_Game.py",     title="Cat Game",      icon="🐱"),
     #st.Page("Pet_Guide_Generator.py",     title="Pet Guide",      icon="📋"),
     st.Page("pet_app_updated.py",     title="Pet Game",      icon="📋"),
+    st.Page("home_app_05_023_modified.py", title="Home Guide Playground", icon="🏡"),
     st.Page("home_app.py",     title="Home Guide",      icon="🏡"),
     st.Page("mychatapp.py",     title="Chat Q&A",      icon="🗣️")
 ]
