faa866d4edaeff9a21c0ed8a6c08c96dcdfdfabb
fishfood65 <167575622+fishfood65@users.noreply.github.com>
2025-05-23 01:39:47 +0000

    updated intro page

--- starting_page.py
+++ starting_page.py
@@ -7,4 +7,12 @@ st.markdown(
     """
     TBM POC is a to be named POC 
 
-    ### Want to try it out? Start by picking a choice from the left nav ba """)
+### Introduction
+
+Goal robust app for easing mental load of organizing, preparing, and collaborating on infrequent but significant events throughout adulthood with others
+- Starting with pet and house care in emergency situations for individuals, families, and loved ones
+- Incrementally incorporating minimally need financial, medical, and tangible asset data
+- Growing into preparation for emergencies of all kinds (i.e. nature, accidental, fraud), estate planning, wills, trusts discussions in an organic natural way
+- Ultimately, assisting in a trust, estate executor with final duties 
+
+#### 👈 Click the Left Navigation Bar To Start """)
